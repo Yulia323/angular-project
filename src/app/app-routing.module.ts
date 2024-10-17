f1920011f77f067fc459a0581ab11c3c70f0357c
Yulia Baluakina <yuliaxbalyakina@gmail.com>
2024-10-17 18:12:19 +0300

    Создала header, настроила роутинг

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CatalogComponent} from './components/catalog/catalog.component';
+import {AboutComponent} from './components/about/about.component';
+import {ContactsComponent} from './components/contacts/contacts.component';
+import {BasketComponent} from "./components/basket/basket.component";
+import {PersonalAccountComponent} from "./components/personal-account/personal-account.component";
+import {MainPageComponent} from "./components/main-page/main-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'basket', component: BasketComponent },
+  { path: 'personal-account', component: PersonalAccountComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { PersonalAccountComponent } from './components/personal-account/personal-account.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BasketComponent,
+    CatalogComponent,
+    AboutComponent,
+    ContactsComponent,
+    PersonalAccountComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
